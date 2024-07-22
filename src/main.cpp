523c818d08d21627a4c48ad1b212c560eef03108
CPudding <150633243+CPuddingOwO@users.noreply.github.com>
2024-07-23 00:01:08 +0800

    init project structure

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <glad/glad.h>
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+#include <cstdio>
+#define GL_SILENCE_DEPRECATION
+#if defined(IMGUI_IMPL_OPENGL_ES2)
+#include <GLES2/gl2.h>
+#endif
+#include <GLFW/glfw3.h>
+
+
+int main(int argc, char *argv[]) {
+    return 0;
+}
